aab6d03963a932ec109114393476164a47f650e5
ISH <70657630+elementISH@users.noreply.github.com>
2024-10-28 01:15:17 +0300

    production

---
+++ src/app/page.jsx
@@ -0,0 +1,69 @@
+"use client";
+import { Picker } from "@/components/ui/picker";
+import { useEffect, useState, useRef } from "react";
+import html2canvas from "html2canvas";
+import { toast } from "sonner";
+
+export default function Home() {
+  const [background, setBackground] = useState("#334155");
+  const captureRef = useRef(null);
+
+  useEffect(() => {
+    if (captureRef.current) {
+      const colorElements = captureRef.current.querySelectorAll(
+        "[data-group='color']"
+      );
+      colorElements.forEach((element) => {
+        element.style.fill = background;
+      });
+    }
+  }, [background]);
+
+  const copyDivToClipboard = async () => {
+    if (!captureRef.current) return;
+
+    try {
+      // Capture the div as a canvas
+      const canvas = await html2canvas(captureRef.current);
+
+      // Convert canvas to a blob and copy to clipboard
+      canvas.toBlob(async (blob) => {
+        if (blob) {
+          await navigator.clipboard.write([
+            new ClipboardItem({ "image/png": blob }),
+          ]);
+          toast.success("image copied to clipboard! Paste to share.");
+        }
+      }, "image/png");
+    } catch (error) {
+      toast.error("Failed to copy image, please try again.");
+    }
+  };
+
+  return (
+    <div className="h-screen w-screen grid place-items-center bg-slate-700 overflow-hidden">
+      <div className="grid place-items-center">
+        <div className="w-[90%] md:w-[30%]">
+          <div className="flex flex-col gap-4">
+            <div
+              className="relative w-full"
+              ref={captureRef}
+              style={{ background: background }}
+            >
+              <img src="Tshirt.png" alt="" />
+            </div>
+            <div>
+              <Picker background={background} setBackground={setBackground} />
+            </div>
+            <button
+              onClick={copyDivToClipboard}
+              className="text-white px-4 py-2 rounded bg-slate-900"
+            >
+              Share Image
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ui/picker.jsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import ColorPicker from "react-color-picker-wheel";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { cn } from "@/lib/utils";
+import { Paintbrush } from "lucide-react";
+import Link from "next/link";
+import { useMemo, useState } from "react";
+import Wheel from "@uiw/react-color-wheel";
+
+export function Picker({ background, setBackground, className }) {
+  const solids = [
+    "#2c5346",
+    "#800220",
+    "#9f88a2",
+    "#7856a0",
+    "#493362",
+    "#A2CFFE",
+    "#92bce2",
+    "#247082",
+    "#101213",
+  ];
+
+  const defaultTab = useMemo(() => {
+    return "solid";
+  }, [background]);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-full justify-start text-left font-normal",
+            !background && "text-muted-foreground",
+            className
+          )}
+        >
+          <div className="w-full flex items-center gap-2">
+            {background ? (
+              <div
+                className="h-4 w-4 rounded !bg-center !bg-cover transition-all"
+                style={{ background }}
+              ></div>
+            ) : (
+              <Paintbrush className="h-4 w-4" />
+            )}
+            <div className="truncate flex-1">
+              {background ? background : "Pick a color"}
+            </div>
+          </div>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-full">
+        <Tabs defaultValue={defaultTab} className="w-full">
+          <TabsList className="w-full mb-4">
+            <TabsTrigger className="flex-1" value="solid">
+              Solid
+            </TabsTrigger>
+            <TabsTrigger className="flex-1" value="wheel">
+              Wheel
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="solid" className="flex flex-wrap gap-1 mt-0">
+            {solids.map((s) => (
+              <div
+                key={s}
+                style={{ background: s }}
+                className="rounded-md h-6 w-6 cursor-pointer active:scale-105"
+                onClick={() => setBackground(s)}
+              />
+            ))}
+          </TabsContent>
+
+          <TabsContent value="wheel" className="mt-0">
+            <div className="flex flex-wrap gap-1 mb-2 justify-items-center justify-center">
+              {/* <ColorPicker
+                initialColor="#FF0000"
+                onChange={(color) => setBackground(color.hex)}
+                size={200}
+              /> */}
+              <Wheel
+                color={background}
+                onChange={(color) => setBackground(color.hex)}
+              />
+            </div>
+          </TabsContent>
+        </Tabs>
+
+        <Input
+          id="custom"
+          value={background}
+          className="col-span-2 h-8 mt-4"
+          onChange={(e) => setBackground(e.currentTarget.value)}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
